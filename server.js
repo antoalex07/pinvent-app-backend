cec2a6702be6ec605485ba71ae1f4041f8913857
Anto Alex <82249465+antoalex07@users.noreply.github.com>
2023-04-14 12:31:39 +0530

    URL changes

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cors({
-    origin: ["http://localhost:3000", "https://pinvent-app-antoalex07.onrender.com/"],
+    origin: ["http://localhost:3000", "https://pinvent-app-antoalex07.onrender.com"],
     credentials: true
 }));
 
